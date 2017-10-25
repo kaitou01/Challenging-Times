3ca0268ce1e539cbca56579500c81f3b1082c924
Kenson Catibog <kensoncatibog23@gmail.com>
2017-10-25 17:25:45 +0000

    changed the URL for the about page from about to about_us

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home', as: 'home'
 
-  get 'about', to: 'pages#about', as: 'about'
+  get 'about_us', to: 'pages#about', as: 'about'
 end
