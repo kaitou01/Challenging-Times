2b763cb4b0eda82ad931d8922f1dcf88a8ccd49e
Kenson Catibog <kensoncatibog23@gmail.com>
2017-10-26 18:48:55 +0000

    Added 100 db entries using faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Pet.create(name: Faker::Name.name, food: Faker::Food.dish)
+end
